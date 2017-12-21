9670ac776661e8bbb47b2d884aad10e1b6aeb1b4
tanvir hasan <tanvir002700@gmail.com>
2017-12-22 01:29:03 +0600

    add test spec for length

--- lib/convert_unit/base.rb
+++ lib/convert_unit/base.rb
@@ -3,7 +3,7 @@ class Base
 
   def initialize(value, unit, valid_units)
     raise TypeError, 'no implicit conversion of String into Integer' unless value.is_a? Numeric
-    raise TypeError, 'Invalid type' unless valid_units.include?(unit.to_s.downcase)
+    raise TypeError, 'Invalid Unit Type' unless valid_units.include?(unit.to_s.downcase)
     @value = value
     @unit = unit.downcase
   end
--- spec/convert_unit/length_spec.rb
+++ spec/convert_unit/length_spec.rb
@@ -1,7 +1,27 @@
 module ConvertUnit
   RSpec.describe Length do
-    it 'should run' do
-      puts 'run'
+    context 'initiate object with wrong unit type argument' do
+      it 'raise a TypeError' do
+        expect{Length.new(1,2)}.to raise_error(TypeError, 'Invalid Unit Type')
+      end
+    end
+
+    context 'initiate object with worng unit argument' do
+      it 'raise a TypeError' do
+        expect{Length.new(1, 'degre')}.to raise_error(TypeError, 'Invalid Unit Type')
+      end
+    end
+
+    context 'initiate object with wrong value' do
+      it 'raise a TypeError' do
+        expect{Length.new('abc', 'metre')}.to raise_error(TypeError)
+      end
+    end
+
+    context 'initiate object with valid value' do
+      it 'not raise any error' do
+        expect{Length.new(1, 'metre')}.to_not raise_error
+      end
     end
   end
 end
