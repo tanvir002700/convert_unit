02dc4e50463033a7b2677c2b2245c40593fd2d9b
tanvir hasan <tanvir002700@gmail.com>
2017-12-21 23:24:54 +0600

    refactor code

--- lib/convert_unit/base.rb
+++ lib/convert_unit/base.rb
@@ -1,10 +1,10 @@
 class Base
   attr_accessor :value, :unit
 
-  def initialize(value, unit, list)
-    puts "calling superk"
+  def initialize(value, unit, valid_units)
+    raise TypeError, 'no implicit conversion of String into Integer' unless value.is_a? Numeric
+    raise TypeError, 'Invalid type' unless valid_units.include?(unit.to_s.downcase)
     @value = value
-    @unit = unit
+    @unit = unit.downcase
   end
 end
-
--- lib/convert_unit/length.rb
+++ lib/convert_unit/length.rb
@@ -1,19 +1,5 @@
-#module ConvertUnit
-  class Base
-    attr_accessor :value, :unit
-
-    def initialize(value, unit, valid_units)
-      puts "called super"
-      puts value
-      puts unit
-      puts valid_units
-      raise TypeError, 'no implicit conversion of String into Integer' unless value.is_a? Numeric
-      raise TypeError, 'Invalid type' unless valid_units.include?(unit.to_s.downcase)
-      @value = value
-      @unit = unit.downcase
-    end
-  end
-
+require 'convert_unit/base'
+module ConvertUnit
   class Length < Base
     UNITS = ['kilometre', 'metre', 'centemetre', 'millimetre', 'nanometre', 'mile', 'yard', 'foot', 'inch', 'nauticalmile']
 
@@ -23,4 +9,4 @@
       super(value, unit, Length::UNITS)
     end
   end
-#end
+end
