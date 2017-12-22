4bd7d8f7618eaf62765ad962636558036e04988e
tanvir hasan <tanvir002700@gmail.com>
2017-12-22 21:49:09 +0600

    update yml data for length

--- lib/convert_unit/pre_process/length/load_yml.rb
+++ lib/convert_unit/pre_process/length/load_yml.rb
@@ -2,6 +2,11 @@ require 'yaml'
 module ConvertUnit
   module PreProcess
     module Length
+      def load_units_short_form
+        f = File.open(File.join(File.dirname(__FILE__), 'units_short_form.yml'))
+        YAML.load(f)
+      end
+
       def load_units
         f = File.open(File.join(File.dirname(__FILE__), 'units.yml'))
         YAML.load(f)
