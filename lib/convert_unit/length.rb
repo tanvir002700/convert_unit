0a60903a99f180b47df86ec1e3b978447c693d16
tanvir hasan <tanvir002700@gmail.com>
2017-12-25 19:08:22 +0600

    change length class constant name

--- lib/convert_unit/length.rb
+++ lib/convert_unit/length.rb
@@ -4,23 +4,23 @@ require 'convert_unit/pre_process/length/process'
 module ConvertUnit
   class Length < Base
     extend PreProcess::Length
-    UNITS_SHORT_FORM = load_units_short_form
+    UNITS_SYMBOL = load_units_short_form
     UNITS = load_available_units
     CONVERSION_RATE = load_conversion_rate
 
     attr_accessor :value, :unit
 
     def initialize(value, unit)
-      unit_in_short = Length::UNITS_SHORT_FORM[unit] || unit
+      unit_symbol = Length::UNITS_SYMBOL[unit] || unit
       @conversion_rate_for_one_unit = Length::CONVERSION_RATE
-      super(value, unit_in_short, Length::UNITS)
+      super(value, unit_symbol, Length::UNITS)
     end
 
     def to(c_unit)
       c_unit.downcase!
-      unit_in_short = Length::UNITS_SHORT_FORM[c_unit] || c_unit
-      raise ArgumentError, 'Unpossible conversion type' unless Length::UNITS.include?(unit_in_short)
-      Length.new(convert_to(unit_in_short), unit_in_short)
+      unit_symbol = Length::UNITS_SYMBOL[c_unit] || c_unit
+      raise ArgumentError, 'Unpossible conversion type' unless Length::UNITS.include?(unit_symbol)
+      Length.new(convert_to(unit_symbol), unit_symbol)
     end
   end
 end
