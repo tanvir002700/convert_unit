e69501056cbe5779f4971836f5aeb88c12316ff8
tanvir hasan <tanvir002700@gmail.com>
2017-12-25 19:02:54 +0600

    change area class constant name

--- lib/convert_unit/area.rb
+++ lib/convert_unit/area.rb
@@ -4,23 +4,23 @@ require 'convert_unit/pre_process/area/process'
 module ConvertUnit
   class Area < Base
     extend PreProcess::Area
-    UNITS_SHORT_FORM = load_units_short_form
+    UNITS_SYMBOL = load_units_short_form
     UNITS = load_available_units
     CONVERSION_RATE = load_conversion_rate
 
     attr_accessor :value, :unit
 
     def initialize(value, unit)
-      unit_in_short = Area::UNITS_SHORT_FORM[unit] || unit
+      unit_symbol = Area::UNITS_SYMBOL[unit] || unit
       @conversion_rate_for_one_unit = Area::CONVERSION_RATE
-      super(value, unit_in_short, Area::UNITS)
+      super(value, unit_symbol, Area::UNITS)
     end
 
     def to(c_unit)
       c_unit.downcase!
-      unit_in_short = Area::UNITS_SHORT_FORM[c_unit] || c_unit
-      raise ArgumentError, 'Unpossible conversion type' unless Area::UNITS.include?(unit_in_short)
-      Area.new(convert_to(unit_in_short), unit_in_short)
+      unit_symbol = Area::UNITS_SYMBOL[c_unit] || c_unit
+      raise ArgumentError, 'Unpossible conversion type' unless Area::UNITS.include?(unit_symbol)
+      Area.new(convert_to(unit_symbol), unit_symbol)
     end
   end
 end
